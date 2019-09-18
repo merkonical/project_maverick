f99ab8650fbb03fe880331fd3a4846e64d9329d4
Maverick Adolfo <merkonical@gmail.com>
2019-09-18 06:13:33 +0000

    Completed sandbox IDE setup

--- lib/project_maverick.rb
+++ lib/project_maverick.rb
@@ -2,7 +2,8 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require "project_maverick/version"
+#require "./lib/project_maverick/version"
+require_relative "project_maverick/version"
 
 module ProjectMaverick
   class Error < StandardError; end
