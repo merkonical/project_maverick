526d5f93904ad31fba2587a90cf32cde89995af6
Maverick Adolfo <merkonical@gmail.com>
2019-09-18 06:34:11 +0000

    Added CLI and started it

--- lib/project_maverick.rb
+++ lib/project_maverick.rb
@@ -4,6 +4,8 @@ require "pry"
 
 #require "./lib/project_maverick/version"
 require_relative "project_maverick/version"
+require_relative "project_maverick/cli"
+require_relative "project_maverick/Scraper"
 
 module ProjectMaverick
   class Error < StandardError; end
---
+++ lib/project_maverick/cli.rb
@@ -0,0 +1,8 @@
+class CLI 
+  
+  def run 
+    puts "Made a new CLI instance and ran it."
+    Scrape.get_initial_data
+  end 
+  
+end 
